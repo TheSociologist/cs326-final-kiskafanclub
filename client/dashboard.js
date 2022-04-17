60dd88c7d53806d8aed370328a5d168bf6a93f05
Ajan Prabakar <ajanprabakar@gmail.com>
2022-04-16 22:11:27 -0400

    changed up link

--- client/dashboard.js
+++ client/dashboard.js
@@ -6,11 +6,11 @@ const renderFeed = async () => {
     renderPostList(document.getElementById('feed'), posts)
 }
 
-const renderList = (list, items) => {
+const renderList = (list, link, items) => {
     items.forEach(({name, id}) => {
         const item = document.createElement('a')
         item.innerText = name
-        item.href = `/college?id=${id}`
+        item.href = `/${link}.html?id=${id}`
         const div = document.createElement('div')
         div.appendChild(item)
         list.appendChild(div)
@@ -20,20 +20,20 @@ const renderList = (list, items) => {
 const renderSuggestedSchools = async () => {
     const response = await fetch('/recommended-schools')
     const schools = await response.json()
-    renderList(document.getElementById('schools'), schools)
+    renderList(document.getElementById('schools'), 'college', schools)
     
 }
 
 const renderSuggestedTutors = async () => {
     const response = await fetch('/recommended-tutors')
     const tutors = await response.json()
-    renderList(document.getElementById('tutors'), tutors)
+    renderList(document.getElementById('tutors'), 'profile', tutors)
 }
 
 const renderOngoingMeetings = async () => {
     const response = await fetch('/ongoing-meetings')
     const meetings = await response.json()
-    renderList(document.getElementById('meetings'), meetings)
+    renderList(document.getElementById('meetings'), 'meeting', meetings)
 }
 
 renderFeed()
