59f10a69871a762595978dc6e35cd66b5f0f87a9
sawjji <yuqiji@umass.edu>
2022-04-19 23:10:09 -0400

    profileCommitSoil

--- client/profile.js
+++ client/profile.js
@@ -1,15 +1,18 @@
 //save event listener
 //if the form gets saved, alter the profile to whatever was entere dinto the field
-let inputName = document.getElementById('nameInput').value;
-let inputUni = document.getElementById('uniInput').value;
-let inputYOG = document.getElementById('yogInput').value;
-let inputAboutMe = document.getElementById('aboutMeInput').value;
+let inputName = document.getElementById('nameInput');
+let inputUni = document.getElementById('uniInput');
+let inputYOG = document.getElementById('yogInput');
+let inputAboutMe = document.getElementById('aboutMeInput');
 let saveButton = document.getElementById('saveButton');
 
 let profile;
 
 saveButton.addEventListener('click', () => {
-    profile.name = inputName.value
+    profile.name = inputName.value;
+    profile.university = inputUni.value;
+    profile.gradYear = inputYOG.value;
+    profile.aboutMe = inputAboutMe.value;
     // do this with all other fields
     fetch('/profile/update', {
         method: 'PUT',
@@ -24,13 +27,16 @@ saveButton.addEventListener('click', () => {
 
 
 async function getUserData() {
-    const response = await fetch('/profile/read')
-    profile = await response.json()
-    
-    setProfileElements()
+    const response = await fetch('/profile/read');
+    profile = await response.json();
+    setProfileElements();
 }
 
 function setProfileElements() {
     // go through each profile element and set their value to the corresponding value in the profile object
     document.getElementById('user-name').value = profile.name;
+    document.getElementById('school-name').value = profile.university;
+    document.getElementById('yogStudent').value = profile.gradYear;
+    document.getElementById('studentMajor').value = profile.major;
+    document.getElementById('aboutMePara').value = profile.aboutMe;
 }
